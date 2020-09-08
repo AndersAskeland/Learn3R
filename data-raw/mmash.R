cf2250c1e262295b0124d72c38393277817f77d1
AndersAskeland <anders_askeland@hotmail.com>
2020-09-08 11:44:55 +0200

    Forgot to save

--- data-raw/mmash.R
+++ data-raw/mmash.R
@@ -3,7 +3,8 @@
 usethis::use_data(mmash, overwrite = TRUE)
 
 # load packages
-library(here)
+library(here) # Point to wd
+library(fs) # Delete stuff
 
 # Download data
 mmash_link <- "https://physionet.org/static/published-projects/mmash/multilevel-monitoring-of-activity-and-sleep-in-healthy-people-1.0.0.zip"
@@ -12,6 +13,7 @@ download.file(mmash_link, destfile = here("data-raw/mmash-data.zip"))
 
 # Add data to gitignore
 usethis::use_git_ignore(("data-raw/mmash-data.zip"))
+usethis::use_git_ignore(("data-raw/mmash/"))
 
 
 # Unzip
@@ -20,8 +22,14 @@ unzip(here("data-raw/mmash-data.zip"),
       junkpaths = T)
 
 unzip(here("data-raw/MMASH.zip"),
-      exdir = here("data-raw/"),
+      exdir = here("data-raw/mmash"),
       junkpaths = T)
+
+
+# Delete uneeded files
+file_delete(here(c("data-raw/MMASH.zip", "data-raw/mmash-data.zip", "data-raw/LICENSE.txt")))
+
 
 # Read data
 readr::read_csv("data-raw/mmash-data.zip")
+
